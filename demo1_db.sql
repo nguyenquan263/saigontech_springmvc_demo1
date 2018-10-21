c07b3ae120d5f0b924d20e5b2f7e2b715d0695f7
Minh Quan <nguyenquan263@gmail.com>
2018-10-21 19:20:02 +0700

    update ddl

--- demo1_db.sql
+++ demo1_db.sql
@@ -2,7 +2,7 @@
 MySQL Backup
 Source Server Version: 5.5.5
 Source Database: demodb1
-Date: 10/19/2018 09:22:14
+Date: 10/21/2018 19:17:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -13,12 +13,13 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `specializations`;
 CREATE TABLE `specializations` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(150) COLLATE utf8_vietnamese_ci NOT NULL,
+  `Name` varchar(255) NOT NULL,
   `NumberCredit` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UK_st0bjplrx44hqis27rt2mf5vr` (`Name`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 --  Records 
 -- ----------------------------
-INSERT INTO `specializations` VALUES ('21','INTERNET OF THINGS','55'), ('22','ARDUINO','60'), ('23','C#','65'), ('25','JAVA','65'), ('27','JAVA','65');
+INSERT INTO `specializations` VALUES ('1','JAVA','61'), ('2','C#','62'), ('5','BUSINESS ADMINISTRATOR','50'), ('7','ASSEMBLY','90'), ('19','LUA','120'), ('20','SWIFT','70'), ('21','KOTLIN ANDROID','90'), ('22','INTERNATIONAL BUSINESS','56');
